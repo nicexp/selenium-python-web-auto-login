32736ac4560292277b026d5d30609791eab44284
nice_xp <419797567@qq.com>
2017-02-07 21:21:17 +0800

    web auto login

--- WebLogin.py
+++ WebLogin.py
@@ -15,16 +15,16 @@ if (local_time.tm_wday == 5) or (local_time.tm_wday == 6):
 	exit(0)
 	
 driver=webdriver.Ie()
-driver.get("http://web.oa.zulong.com")
+driver.get("http://xxxxxxxxxx.com")
 
 time.sleep(10)
 
 elem_user=driver.find_element_by_id('txtLoginCode')
 elem_user.clear()
-elem_user.send_keys('mengxiangpeng')
+elem_user.send_keys('nice_xp')
 elem_pass=driver.find_element_by_id('txtPwd')
 elem_pass.clear()
-elem_pass.send_keys('270178')
+elem_pass.send_keys('*******')
 elem_login=driver.find_element_by_id('btnLogin')
 elem_login.click()
 
